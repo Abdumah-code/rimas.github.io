66d9aa100978d02017f23d49dc7ffa4fbd9d2b07
abdu <abdumah2021fr@gmail.com>
2023-06-01 00:49:22 +0200

    tdth

--- smtp.js
+++ smtp.js
@@ -10,17 +10,20 @@ document.querySelector('form').addEventListener('submit', function (e) {
     var phone = document.getElementById('phone').value;
     var people = document.getElementById('people').value;
     var diet = document.getElementById('diet').value;
+    var event = document.getElementById('event').value;
   
-    var body = 'Name: ' + name + '\n';
-    body += 'Email: ' + email + '\n';
-    body += 'Mobile Number: ' + phone + '\n';
-    body += 'Antal Gäster: ' + people + '\n';
-    body += 'Preferenser: ' + diet + '\n';
+    var body = 'Namn: ' + name + '<br>';
+    body += 'Email: ' + email + '<br>';
+    body += 'Mobile Number: ' + phone + '<br>';
+    body += 'Antal Gäster: ' + people + '<br>';
+    body += 'Preferenser: ' + diet + '<br>';
+    body += 'Event: ' + event + '<br>';
+    
   
     Email.send({
       SecureToken : "f5040cc6-b5f0-41ae-9f7e-8c272cb338b6",
-      To: 'abdullahtillabd2014@gmail.com',
-      From: "abdullahtillabd2014@gmail.com",
+      To: 'info@rimascatering.com',
+      From: "info@rimascatering.com",
       Subject: "Form Submission",
       Body: body
     }).then(function (message) {
